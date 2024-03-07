67e01be8ba63a1cbb36e1d50ef7f99787e78f3be
Titouan Massé <titouanmasselaude@gmail.com>
2024-03-07 10:15:12 +0100

    Amélioration d'animation lors du drag and drop de la cards

--- cards.js
+++ cards.js
@@ -3,9 +3,13 @@ const cards = document.querySelectorAll('.card');
 const onDragStart = (event) => {
     console.log('dragging the element');
     event.dataTransfer.setData('id',event.target.id);
+    setTimeout(() =>{
+        event.target.style.visibility = 'hidden';
+    },50)
 }
 
 const onDragEnd = (event) => {
+    event.target.style.visibility = 'visible'
     console.log('ended the drag');
 }
 
